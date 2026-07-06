a5daa2a214ba25ec707fe055538b55f6ad842d6e
Niavand <mayhie50@gmail.com>
2026-07-06 16:50:33 +0330

    feat: add proper message in exception handler for race condition

--- VolunteerTaskManagement.Application/CQRS/Tasks/Query/GetMyList/TaskListDTO.cs
+++ VolunteerTaskManagement.Application/CQRS/Tasks/Query/GetMyList/TaskListDTO.cs
@@ -21,7 +21,7 @@ namespace VolunteerTaskManagement.Application.CQRS.Tasks
         public string? NeighborhoodTitle { get; set; }
         public string? Address { get; set; }
         public DateTime StartDate { get; set; }
-        public string StartDateFa => StartDate.ToPersianDateTime().ToString();
+        public string StartDateFa => StartDate.ToPersianDateTime().ToShortDateString();
         public string? RegionName { get; set; }
         public string? CityName { get; set; }
         public bool IsAssigned { get; set; }
