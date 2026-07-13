61e2d0bcb35ce302fae251b44ba2f4b59a39ddd6
Niavand <mayhie50@gmail.com>
2026-07-13 23:55:59 +0330

    feat: add notif count api

---
+++ VolunteerTaskManagement.Application/CQRS/NotficationLogs/Query/GetCount/NotificationLogGetCountQuery.cs
@@ -0,0 +1,24 @@
+﻿using Base.Application.Contracts;
+using Base.Application.Contracts.DTOs.Common;
+using MediatR;
+using VolunteerTaskManagement.Application.Contracts;
+
+namespace VolunteerTaskManagement.Application.CQRS.NotficationLogs.Query.GetCount
+{
+    public class NotificationLogGetCountQuery : IRequest<Result>
+    {
+    }
+
+    public class NotificationLogGetCountQueryHandler(IVolunteerTaskManagementUnitOfWork uow, IJwtManager jwtManager)
+        : IRequestHandler<NotificationLogGetCountQuery, Result>
+    {
+        public async Task<Result> Handle(NotificationLogGetCountQuery request, CancellationToken cancellationToken)
+        {
+            var userId = jwtManager.GetUserId();
+
+            var unSeenNotifCount = await uow.NotificationLogs.CountAsync(x => !x.IsSeen && x.UsersId.Contains(userId.Value));
+
+            return Result.Success(unSeenNotifCount);
+        }
+    }
+}
